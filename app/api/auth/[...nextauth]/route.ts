95fc5de8e9c9736801456b7b0fa90e7e6a0d4383
developervicky <developer.vicky6@gmail.com>
2024-03-16 01:29:50 +0530

    fix: prisma production issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -42,7 +42,7 @@ export const authOptions: AuthOptions = {
 
         const isCorrectPassword = await bcrypt.compare(
           credentials.password,
-          user.hashedPassword
+          user.hashedPassword,
         );
 
         if (!isCorrectPassword) {
